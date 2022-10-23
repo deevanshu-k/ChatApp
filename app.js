c1fe7c0ad0752b512c66f96cedd0b3e166d6037e
Deevanshu kushwah <astroboiscosmos@gmail.com>
2022-10-23 15:38:39 +0530

    h5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
-        origin: ['https://deevanshu-k.github.io/ChatApp']
+        origin: ['https://deevanshu-k.github.io']
     }
 });
 
