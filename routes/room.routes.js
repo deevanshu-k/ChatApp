3f9d9d0b2d99980a5513d664c7bae73c5374d937
Deevanshu kushwah <astroboiscosmos@gmail.com>
2022-10-26 22:59:03 +0530

    POST: /getRoomAccess  Created

--- routes/room.routes.js
+++ routes/room.routes.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const { get_all_rooms } = require("../controller/room.controller");
+const {post_room_access} = require('../controller/auth.controller');
 const router = Router();
 
 router.get('/rooms',get_all_rooms);
+router.post('/getRoomAccess',post_room_access);
 
 module.exports = router ; 
