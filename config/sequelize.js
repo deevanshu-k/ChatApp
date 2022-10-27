33379f2a0404803738bc201ecf30d51e7573816c
Deevanshu kushwah <astroboiscosmos@gmail.com>
2022-10-27 21:46:01 +0530

    Ready to deploy

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,17 +1,20 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/config.json')[env];
-
-
-
-const db = new Sequelize(
-	config.database,
-	config.username,
-	config.password, {
-		dialect: config.dialect,    		
+let db;
+if (env == 'production') {
+	db = new Sequelize(process.env.DATABASE_URL);
+}
+else {
+	db = new Sequelize(
+		config.database,
+		config.username,
+		config.password, {
+		dialect: config.dialect,
 		host: config.host,
-		logging : config.logging
+		logging: config.logging
 	}
-);
+	);
+}
 
-module.exports = {db};
+module.exports = { db };
